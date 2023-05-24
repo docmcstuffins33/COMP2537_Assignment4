c07f6e71adb7068cc37265b067b40b4c4d3d2a33
Mylo Yu <emdoubleu.yu@gmail.com>
2023-05-23 20:09:12 -0700

    Added powerups and timer. feiwvbiyrbvgiuwebjkew

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ let flipping = false
 let firstCard = undefined
 let secondCard = undefined
 let cardsFlipped = 0
-let cardAmount = 6
+let cardAmount = 0
 let clicks = 0
 let pokemons = [];
 let pairsMatched = 0
-let pairsLeft = cardAmount/2
+let pairsLeft = 0
+let seconds = 0
+let timer = undefined
 
 const setup = async () => {
     $('#game_grid').empty()
@@ -16,7 +18,52 @@ const setup = async () => {
         location.reload();
     })
 
+
+    $("#restart").on(("click"), function () {
+        location.reload();
+    })
+
+    function second() {
+        seconds++
+        timer = setTimeout(second, 1000)
+        if(Math.floor(Math.random() * 10) == 0 && firstCard == undefined && secondCard == undefined && !flipping) {
+            console.log("powerup")
+            flipping = true
+            alert("Powerup!!!")
+            $(".card").toggleClass("flip");
+            flipping = true
+              console.log("no match")
+              setTimeout(() => {
+                $(`.card`).toggleClass("flip")
+                flipping = false
+              }, 2000)
+        }
+        $('#stats').empty()
+        $('#stats').append(`<h2>Time: ${seconds} seconds</h2>
+        <h2>Number of clicks: ${clicks}</h2>
+        <h2>Pairs left: ${pairsLeft}</h2>
+        <h2>Pairs matched: ${pairsMatched}/${cardAmount/2}</h2>`)
+    }
+
     $("#start").on(("click"), async function () {
+        timer = setTimeout(second, 1000)
+        var diff = document.getElementById('difficulty')
+
+        console.log(diff.value)
+        
+        switch (diff.value) {
+            case ("Easy"): cardAmount = 6
+            break;
+            case ("Medium"): cardAmount = 12
+            break;
+            case ("Hard"): cardAmount = 18
+            break;
+            default: cardAmount = 6
+        }
+        pairsLeft = cardAmount/2
+        $(this).off("click")
+        const grid = document.getElementById("game_grid");
+        grid.setAttribute("style",`height:${200*cardAmount/3}px;`)
         var lastRandom = [-999]
         var random = -999
         for(i = 0; i < cardAmount/2; i++) {
@@ -39,6 +86,12 @@ const setup = async () => {
             </div>`)
             pokemons.splice(random, 1)
         }
+        $("#dark").on(("click"), function () {
+            grid.setAttribute("style",`height:${200*cardAmount/3}px;background-color: black`)
+        })
+        $("#light").on(("click"), function () {
+            grid.setAttribute("style",`height:${200*cardAmount/3}px;background-color: beige`)
+        })
         $(".card").on(("click"), function () {
             if(firstCard != $(this).find(".front_face")[0] && !flipping) {
                 clicks++;
@@ -64,6 +117,7 @@ const setup = async () => {
               pairsLeft--
               if(cardsFlipped == cardAmount) {
                 alert("You win :D");
+                clearTimeout(timer);
               }
             }
             else{
@@ -80,17 +134,19 @@ const setup = async () => {
             }
           }
           $('#stats').empty()
-            $('#stats').append(`<h2>Number of clicks: ${clicks}</h2>
-            <h2>Pairs left: ${pairsLeft}</h2>
-            <h2>Pairs matched: ${pairsMatched}/${cardAmount/2}`)
+          $('#stats').append(`<h2>Time: ${seconds} seconds</h2>
+          <h2>Number of clicks: ${clicks}</h2>
+          <h2>Pairs left: ${pairsLeft}</h2>
+          <h2>Pairs matched: ${pairsMatched}/${cardAmount/2}</h2>`)
         }
         });
+        $('#stats').append(`<h2>Time: ${seconds} seconds</h2>
+        <h2>Number of clicks: ${clicks}</h2>
+        <h2>Pairs left: ${pairsLeft}</h2>
+        <h2>Pairs matched: ${pairsMatched}/${cardAmount/2}</h2>`)
     })
 
    
-    $('#stats').append(`<h2>Number of clicks: ${clicks}</h2>
-    <h2>Pairs left: ${pairsLeft}</h2>
-    <h2>Pairs matched: ${pairsMatched}/${cardAmount/2}`)
   }
   
   $(document).ready(setup)
